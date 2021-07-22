aa2610d4ec16793f04bb7ef511c358d4ceb3867a
Vladimir Stratiev <vladistratiev@vladis-mac.local>
2021-07-22 17:03:04 +0300

    Security service initial commit

---
+++ securityapp/src/main/java/com/peerLending/securityapp/user/model/UserRepository.java
@@ -0,0 +1,6 @@
+package com.peerLending.securityapp.user.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
